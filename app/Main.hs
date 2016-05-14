02ad3480823150e476fe33da68dedb141fd1e892
zjhmale <zjhmale@gmail.com>
2016-05-14 13:37:17 +0800

    propagation global env in repl

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,21 @@ module Main where
 import Eval (eval)
 import Infer (analyze)
 import Parser (parseExpr)
-import Value (ValueScope)
-import TypeScope (TypeScope)
+import Value (ValueScope(..))
+import TypeScope (TypeScope(..))
 import Prologue (assumptions, builtins)
 import Control.Lens
 import Control.Monad.Trans
 import System.Environment
 import System.Console.Haskeline
+import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Control.Exception as E
 
-loadlib :: IO (TypeScope, ValueScope)
+type Env = (TypeScope, ValueScope)
+emptyEnv = (TypeScope Nothing M.empty, ValueScope Nothing M.empty)
+
+loadlib :: IO Env
 loadlib = do
   assumps <- assumptions
   std <- readFile "./lib/std.ntha"
@@ -22,30 +26,36 @@ loadlib = do
   let (stdbuiltins, _) = eval stdast builtins
   return (stdassumps, stdbuiltins)
 
-process :: String -> IO ()
-process expr = E.catch (do
-                        (stdassumps, stdbuiltins) <- loadlib
+process :: Env -> String -> IO Env
+process (assumps, builtins) expr = E.catch (do
                         let ast = parseExpr expr
-                        (_, t) <- analyze ast stdassumps S.empty
-                        let (_, v) = eval ast stdbuiltins
-                        putStrLn $ show v ++ " : " ++ show t)
-                       (\(E.ErrorCall e) -> putStrLn e)
+                        (assumps', t) <- analyze ast assumps S.empty
+                        let (builtins', v) = eval ast builtins
+                        putStrLn $ show v ++ " : " ++ show t
+                        return (assumps', builtins'))
+                       (\(E.ErrorCall e) -> do
+                        putStrLn e
+                        return emptyEnv)
 
-loop :: InputT IO ()
-loop = do
+loop :: Env -> InputT IO Env
+loop env = do
   minput <- getInputLine "λ> "
   case minput of
-    Nothing -> outputStrLn "Goodbye."
-    Just input -> (liftIO $ process input) >> loop
+    Nothing -> do
+      outputStrLn "Goodbye."
+      return emptyEnv
+    Just input -> (liftIO $ process env input) >>= (\env -> loop env)
 
-main :: IO ()
+main :: IO Env
 main = do
-  loadlib
+  env <- loadlib
   args <- getArgs
   case (args ^? element 0) of
     Just arg -> if arg == "repl"
-               then runInputT defaultSettings loop
-               else putStrLn "unsupported mode"
+               then runInputT defaultSettings (loop env)
+               else do
+                putStrLn "unsupported mode"
+                return emptyEnv
     Nothing -> do
       input <- getContents
-      process input
+      process env input
